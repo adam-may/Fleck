2051d25d50b6a907ed37c6f142d9bc05ac089cdc
Adam May <adammay@gmail.com>
2013-05-02 08:01:29 +1000

    Adding custom application delegate handling (register/deregister delegates for decoding CALL messages)

--- src/Fleck.Wamp/WampSubProtocolHandler.cs
+++ src/Fleck.Wamp/WampSubProtocolHandler.cs
@@ -30,9 +30,15 @@ namespace Fleck
     {
         private const int PROTOCOL_VERSION = 1;
         private readonly string _serverIdentity;
+        // Maps connection id to real connection
         private readonly IDictionary<Guid, IWebSocketConnection> _connections;
+        // Per-user list of prefixes sent to the service
         private readonly IDictionary<Guid, IDictionary<string, string>> _prefixes;
+        // List of connection ids for each topic
         private readonly IDictionary<Uri, IList<Guid>> _subscriptions;
+        // Lookup for application specific end points. First item in Tuple is the type to be deserialised, 
+        // second item is the piece of code to run to handle the call
+        private readonly IDictionary<Uri, Tuple<Type, Action<object>>> _registeredDelegates;
 
         public Action<IWebSocketConnection> OnWelcomeMessage { get; set; }
         public Action<IWebSocketConnection, string, string> OnPrefixMessage { get; set; }
@@ -43,17 +49,13 @@ namespace Fleck
         public Action<IWebSocketConnection, Uri> OnUnsubscribeMessage { get; set; }
         public Action<IWebSocketConnection, Uri, string, IEnumerable<Guid>, IEnumerable<Guid>> OnPublishMessage { get; set; }
         public Action<IWebSocketConnection, Uri, string> OnEventMessage { get; set; }
-        
-        public IDictionary<Guid, IWebSocketConnection> Connections
-        {
-            get { return _connections; }
-        }
 
         public WampSubProtocolHandler()
         {
             _connections = new Dictionary<Guid, IWebSocketConnection>();
             _prefixes = new Dictionary<Guid, IDictionary<string, string>>();
             _subscriptions = new Dictionary<Uri, IList<Guid>>();
+            _registeredDelegates = new Dictionary<Uri, Tuple<Type, Action<object>>>();
 
             var assemblyName = Assembly.GetExecutingAssembly().GetName();
             _serverIdentity = String.Format("{0}/{1}.{2}.{3}",
@@ -71,6 +73,24 @@ namespace Fleck
             OnUnsubscribeMessage = (conn, topicUri) => { };
             OnPublishMessage = (conn, topicUri, eventId, exclude, eligible) => { };
             OnEventMessage = (conn, topicUri, eventId) => { };
+        }
+
+        #region Dynamic delegates
+        public void RegisterDelegateForMessage<T>(Uri uri, Action<T> d)
+        {
+            _registeredDelegates.Add(uri, Tuple.Create<Type, Action<object>>(typeof(T), o => d((T)o)));
+        }
+
+        public void DeregisterDelegateForMessage(Uri uri)
+        {
+            _registeredDelegates.Remove(uri);
+        }
+        #endregion
+
+        #region ISubProtocolHandler implementation
+        public IDictionary<Guid, IWebSocketConnection> Connections
+        {
+            get { return _connections; }
         }
 
         public string Identifier
@@ -103,7 +123,9 @@ namespace Fleck
                 };
             }
         }
+        #endregion
 
+        #region Message Senders
         private void SendWelcomeMessage(IWebSocketConnection connection)
         {
             object[] parameters = new object[]
@@ -119,50 +141,79 @@ namespace Fleck
             OnWelcomeMessage(connection);
         }
 
-        private void ParseAndHandleMessage(IWebSocketConnection conn, string message)
+        public void SendCallResultMessage(IWebSocketConnection connection, string callId, string result)
         {
-            var parsedMessage = JsonConvert.DeserializeObject<object[]>(message);
-            WampMessageTypeId messageType;
-            
-            
-            if (Enum.TryParse<WampMessageTypeId>(parsedMessage[0].ToString(), out messageType))
+            object[] parameters = new object[]
             {
-                switch (messageType)
-                {
-                    case WampMessageTypeId.Prefix:
-                        // Handle prefix message
-                        HandlePrefixMessage(conn, parsedMessage);
-                        break;
-                    case WampMessageTypeId.Call:
-                        // Handle call message
-                        break;
-                    case WampMessageTypeId.Subscribe:
-                        // Handle subscriptions
-                        HandleSubscribeMessage(conn, parsedMessage);
-                        break;
-                    case WampMessageTypeId.Unsubscribe:
-                        // Handle unsubscriptions
-                        HandleUnsubscribeMessage(conn, parsedMessage);
-                        break;
-                    case WampMessageTypeId.Publish:
-                        // Handle Publishing of messages
-                        HandlePublishMessage(conn, parsedMessage);
-                        break;
-                    case WampMessageTypeId.Event:
-                        HandleEventMessage(conn, parsedMessage);
-                        // Handle sending of events 
-                        break;
-                    case WampMessageTypeId.Welcome:
-                    case WampMessageTypeId.CallResult:
-                    case WampMessageTypeId.CallError:
-                    default:
-                        // Shouldn't receive any of these messages
-                        FleckLog.Info(String.Format("Received bad message on {0}: {1}", conn.ConnectionInfo.Id, message));
-                        break;
-                }
-            }
+                WampMessageTypeId.CallResult,
+                callId,
+                result
+            };
+            var callResultMessage = JsonConvert.SerializeObject(parameters);
+            connection.Send(callResultMessage);
+            FleckLog.Debug(String.Format("Sent CallResult message: {0}", callResultMessage));
+            OnCallResultMessage(connection, callId, result);
         }
 
+        public void SendCallErrorMessage(IWebSocketConnection connection, string callId, string errorUri, string errorDescription, string errorDetails = null)
+        {
+            object[] parameters = null;
+            
+            if (errorDetails == null)
+            {
+                parameters = new object[]
+                {
+                    WampMessageTypeId.CallResult,
+                    callId,
+                    errorUri,
+                    errorDescription
+                };
+            }
+            else
+            {
+                parameters = new object[]
+                {
+                    WampMessageTypeId.CallResult,
+                    callId,
+                    errorUri,
+                    errorDescription,
+                    errorDetails
+                };
+            }
+
+            var callErrorMessage = JsonConvert.SerializeObject(parameters);
+            connection.Send(callErrorMessage);
+            FleckLog.Debug(String.Format("Sent CallError message: {0}", callErrorMessage));
+            OnCallErrorMessage(connection, callId, errorUri, errorDescription, errorDetails);
+        }
+
+        public void SendEventMessage(IWebSocketConnection connection, string topicUri, string eventId)
+        {
+            var uri = new Uri(ExpandPrefix(connection, topicUri));
+
+            object[] parameters = new object[]
+            {
+                WampMessageTypeId.Event,
+                uri.ToString(),
+                eventId
+            };
+            var eventMessage = JsonConvert.SerializeObject(parameters);
+
+            if (_subscriptions.ContainsKey(uri))
+            {
+                _subscriptions[uri].ToList()
+                                   .ForEach(guid =>
+                                       {
+                                           if (_connections.ContainsKey(guid))
+                                               _connections[guid].Send(eventMessage);
+                                       });
+            }
+            FleckLog.Debug(String.Format("Sent Event message: {0}", eventMessage));
+            OnEventMessage(connection, uri, eventId);
+        }
+        #endregion
+
+        #region Message Handlers
         private void HandlePrefixMessage(IWebSocketConnection conn, object[] parameters)
         {
             if (parameters.Length != 3)
@@ -229,7 +280,7 @@ namespace Fleck
 
         private void HandlePublishMessage(IWebSocketConnection conn, object[] parameters)
         {
-            if (parameters.Length != 5)
+            if (parameters.Length < 3 || parameters.Length > 5)
             {
                 FleckLog.Info(String.Format("Received bad publish message on {0}", conn.ConnectionInfo.Id));
                 return;
@@ -237,21 +288,35 @@ namespace Fleck
 
             var topicUri = new Uri(ExpandPrefix(conn, parameters[1].ToString()));
             var eventId = parameters[2].ToString();
-            var excludeList = parameters[3] as JArray;
+
             IEnumerable<Guid> excludeListEnumerable = null;
-            if (excludeList != null)
-            {
-                excludeListEnumerable = excludeList.Select(x => new Guid(x.Value<string>()));
-            }
-            var eligibleList = parameters[4] as JArray;
             IEnumerable<Guid> eligibleListEnumerable = null;
-            if (eligibleList != null)
+            switch (parameters.Length)
             {
-                eligibleListEnumerable = eligibleList.Select(x => new Guid(x.Value<string>()));
-            }
-            else
-            {
-                eligibleListEnumerable = _subscriptions[topicUri];
+                case 3:
+                    excludeListEnumerable = new List<Guid>() { };
+                    eligibleListEnumerable = _subscriptions[topicUri];
+                    break;
+                case 4:
+                    excludeListEnumerable = new List<Guid>() { conn.ConnectionInfo.Id };
+                    eligibleListEnumerable = _subscriptions[topicUri];
+                    break;
+                case 5:
+                    var excludeList = parameters[3] as JArray;
+                    if (excludeList != null)
+                    {
+                        excludeListEnumerable = excludeList.Select(x => new Guid(x.Value<string>()));
+                    }
+                    var eligibleList = parameters[4] as JArray;
+                    if (eligibleList != null)
+                    {
+                        eligibleListEnumerable = eligibleList.Select(x => new Guid(x.Value<string>()));
+                    }
+                    else
+                    {
+                        eligibleListEnumerable = _subscriptions[topicUri];
+                    }
+                    break;
             }
 
             eligibleListEnumerable.Where(guid => !excludeListEnumerable.Contains(guid))
@@ -261,7 +326,7 @@ namespace Fleck
                                           if (_connections.ContainsKey(guid))
                                           {
                                               var connection = _connections[guid];
-                                              // TODO: serialise message
+                                              // TODO: deserialise message
                                               connection.Send("Publish");
                                           }
                                       });
@@ -269,9 +334,47 @@ namespace Fleck
             FleckLog.Info(String.Format("Published message for topic {0}, event {1}", topicUri, eventId));
             OnPublishMessage(conn, topicUri, eventId, excludeListEnumerable, eligibleListEnumerable);
         }
+        #endregion
 
-        private void HandleEventMessage(IWebSocketConnection conn, object[] parameters)
+        #region Utilities
+        private void ParseAndHandleMessage(IWebSocketConnection conn, string message)
         {
+            var parsedMessage = JsonConvert.DeserializeObject<object[]>(message);
+            WampMessageTypeId messageType;
+
+            if (Enum.TryParse<WampMessageTypeId>(parsedMessage[0].ToString(), out messageType))
+            {
+                switch (messageType)
+                {
+                    case WampMessageTypeId.Prefix:
+                        // Handle prefix message
+                        HandlePrefixMessage(conn, parsedMessage);
+                        break;
+                    case WampMessageTypeId.Call:
+                        // Handle call message
+                        break;
+                    case WampMessageTypeId.Subscribe:
+                        // Handle subscriptions
+                        HandleSubscribeMessage(conn, parsedMessage);
+                        break;
+                    case WampMessageTypeId.Unsubscribe:
+                        // Handle unsubscriptions
+                        HandleUnsubscribeMessage(conn, parsedMessage);
+                        break;
+                    case WampMessageTypeId.Publish:
+                        // Handle Publishing of messages
+                        HandlePublishMessage(conn, parsedMessage);
+                        break;
+                    case WampMessageTypeId.Welcome:
+                    case WampMessageTypeId.CallResult:
+                    case WampMessageTypeId.CallError:
+                    case WampMessageTypeId.Event:
+                    default:
+                        // Shouldn't receive any of these messages
+                        FleckLog.Info(String.Format("Received bad message on {0}: {1}", conn.ConnectionInfo.Id, message));
+                        break;
+                }
+            }
         }
 
         private string ExpandPrefix(IWebSocketConnection conn, string uri)
@@ -298,5 +401,6 @@ namespace Fleck
 
             return uri.Replace(components[0] + ":", _prefixes[connId][components[0]]);
         }
+        #endregion
     }
 }
