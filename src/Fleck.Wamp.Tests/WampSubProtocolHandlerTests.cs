e445fe24aa7d1d55b0a70001a7d8552dd341f185
Adam May <adam.may@tibra.com>
2013-05-02 19:08:29 +1000

    Adding tests to the Fleck.Wamp.Tests suite

--- src/Fleck.Wamp.Tests/WampSubProtocolHandlerTests.cs
+++ src/Fleck.Wamp.Tests/WampSubProtocolHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
@@ -11,33 +12,103 @@ namespace Fleck.Wamp.Tests
     [TestFixture]
     public class WampSubProtocolHandlerTests
     {
-        private Mock<IWebSocketServer> _webSocketServer;
-        private IEnumerable<ISubProtocolHandler> _subProtocolInitializers;
+        private Guid _connectionId = new Guid("CFCAB391-2567-4DDE-844F-C44231CA8605");
+        private string _applicationIdentifier = "Fleck.Wamp/0.9.6";
+
+        private Mock<IWebSocketConnectionInfo> _webSocketConnectionInfo;
+        private Mock<IWebSocketConnection> _webSocketConnection;
         private WampSubProtocolHandler _wampSubProtocolHandler;
 
         [SetUp]
         public void Setup()
         {
+            _webSocketConnectionInfo = new Mock<IWebSocketConnectionInfo>();
+            _webSocketConnectionInfo.SetupGet(x => x.Id).Returns(_connectionId);
+            _webSocketConnection = new Mock<IWebSocketConnection>();
+            _webSocketConnection.SetupGet(x => x.ConnectionInfo).Returns(_webSocketConnectionInfo.Object);
             _wampSubProtocolHandler = new WampSubProtocolHandler();
-
-            _webSocketServer = new Mock<IWebSocketServer>("ws://localhost:8080");
-            _webSocketServer.Setup(
-                x => x.Start(It.IsAny<ISubProtocolHandler>(), It.IsAny<IEnumerable<ISubProtocolHandler>>()))
-                            .Returns(x => x);
-            _subProtocolInitializers = new List<ISubProtocolHandler>() {_wampSubProtocolHandler};
         }
 
         [Test]
         public void ShouldReturnProperIdentifier()
         {
+            // Assert
             Assert.IsTrue(_wampSubProtocolHandler.Identifier.Equals("wamp"));
         }
 
         [Test]
-        public void ShouldCallOnWelcome()
+        public void ShouldCallOnWelcomeWhenClientConnects()
         {
-            _webSocketServer.Start(_wampSubProtocolHandler, _subProtocolInitializers);
-            
+            // Arrange
+            var welcomeCalled = false;
+            string message = string.Empty;
+            string expectedWelcomeMessage = String.Format("[0,\"{0}\",1,\"{1}\"]", _connectionId, _applicationIdentifier);
+
+            _webSocketConnection.SetupAllProperties();
+            _webSocketConnection.Setup(x => x.Send(It.IsAny<string>())).Callback<String>(m => message = m);
+
+            _wampSubProtocolHandler.OnWelcomeMessage += conn => { welcomeCalled = true; };
+            _wampSubProtocolHandler.SubProtocolInitializer(_webSocketConnection.Object);
+
+            // Act
+            _webSocketConnection.Object.OnOpen();
+
+            // Assert
+            Assert.IsTrue(welcomeCalled);
+            Assert.IsTrue(message.Equals(expectedWelcomeMessage));
+        }
+
+        [Test]
+        public void ShouldManipulateCollectionCorrectlyWhenClientConnectsOrDisconnects()
+        {
+            // Arrange
+            _webSocketConnection.SetupAllProperties();
+            _wampSubProtocolHandler.SubProtocolInitializer(_webSocketConnection.Object);
+
+            // Act
+            _webSocketConnection.Object.OnOpen();
+
+            // Assert
+            Assert.IsTrue(_wampSubProtocolHandler.Connections.Count == 1);
+            Assert.IsTrue(_wampSubProtocolHandler.Connections.ContainsKey(_connectionId));
+
+            // Act
+            _webSocketConnection.Object.OnClose();
+
+            // Assert
+            Assert.IsTrue(_wampSubProtocolHandler.Connections.Count == 0);
+        }
+
+        [Test]
+        public void ShouldAddPrefixToCollection()
+        {
+            // Arrange
+            var prefixCalled = false;
+            var intendedPrefix = "keyvalue";
+            var intendedUri = "http://example.com/simple/keyvalue#";
+            var prefixMessage = String.Format("[1, \"{0}\", \"{1}\"]", intendedPrefix, intendedUri);
+            var returnedPrefix = String.Empty;
+            var returnedUri = String.Empty;
+
+            _webSocketConnection.SetupAllProperties();
+            _wampSubProtocolHandler.SubProtocolInitializer(_webSocketConnection.Object);
+
+            _webSocketConnection.Object.OnOpen();
+
+            _wampSubProtocolHandler.OnPrefixMessage += (conn, prefix, uri) =>
+                {
+                    prefixCalled = true;
+                    returnedPrefix = prefix;
+                    returnedUri = uri;
+                };
+
+            // Act
+            _webSocketConnection.Object.OnMessage(prefixMessage);
+
+            // Assert
+            Assert.IsTrue(prefixCalled);
+            Assert.IsTrue(returnedPrefix.Equals(intendedPrefix));
+            Assert.IsTrue(returnedUri.Equals(intendedUri));
         }
     }
 }
